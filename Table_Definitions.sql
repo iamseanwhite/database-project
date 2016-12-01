b6c8962491c5a8aa44e219c24f088aaf6bb29e86
Sean White <seanwhite211@gmail.com>
2016-11-30 23:31:37 -0500

    fixed typo on social_media_content table

--- Table_Definitions.sql
+++ Table_Definitions.sql
@@ -5,7 +5,7 @@ field VARCHAR(255) NOT NULL,
 location VARCHAR(255) NOT NULL
 )ENGINE=InnoDB;
 
-Create table social_madia_platform(
+Create table social_media_platform(
 id int PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(255) NOT NULL
 )ENGINE=InnoDB;
