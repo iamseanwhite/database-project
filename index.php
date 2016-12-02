20a8796e44fc2e9eaba4251124fee8f32c86f0fb
Berry <bsemexan@gmail.com>
2016-12-02 15:34:34 -0500

    formatted tables to look cleaner

--- index.php
+++ index.php
@@ -8,14 +8,20 @@ if($mysqli->connect_errno){
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <style type="text/css">
+    fieldset {
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+    }
+
     form label {
         float: left;
         width: 150px;
-        margin-bottom: 5px;
-        margin-top: 5px;
+        margin: 5px;
+
 
     }
     .clear {
@@ -23,15 +29,7 @@ if($mysqli->connect_errno){
         clear: both;
         width: 100%;
     }
-    fieldset {
-        -webkit-box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        box-sizing: border-box;
-    }
 
-    legend{
-        padding: 10px;
-    }
 
     select{
         padding: 5px;
@@ -44,270 +42,289 @@ if($mysqli->connect_errno){
         height: 3%;
         border: 1px solid;
     }
+
+     table, th, td{
+
+         padding: 8px;
+    }
+    tr:nth-child(even) {background-color: #f2f2f2}
+    th {
+        background-color: rgba(44, 44, 44, 0.79);
+        color: white;
+    }
+
+
 </style>
 <body>
-<div>
-	<table style = "float: left">
-		<tr>
-			<td>Businesses</td>
-		</tr>
-		<tr>
-			<td>Name</td>
-			<td>Field</td>
-			<td>Location</td>
-		</tr>
-		<tr>
-			<td> </td>
-			<td> </td>
-			<td> </td>
-		</tr>
-		
-			
-			<?php
-			if(!($stmt = $mysqli->prepare("SELECT business.name, business.field, business.location FROM business"))){
-				echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
-			}
-
-			if(!$stmt->execute()){
-				echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-			}
-			if(!$stmt->bind_result($name, $field, $location)){
-				echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-			}
-			while($stmt->fetch()){
-			 echo "<tr>\n<td>\n" . $name . "\n</td>\n<td>\n" . $field . "\n</td>\n<td>\n" . $location . "\n</td>\n</tr>";
-			}
-			$stmt->close();
-			?>
-	</table>
-</div>
-
-<div>
-	<table style = "float: left">
-		<tr>
-			<td>Social Media Platform</td>
-		</tr>
-		<tr>
-			<td>Name</td>
-		</tr>
-		<tr>
-			<td> </td>
-			<td> </td>
-			<td> </td>
-		</tr>
-		
-			
-			<?php
-			if(!($stmt = $mysqli->prepare("SELECT social_media_platform.name FROM social_media_platform"))){
-				echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
-			}
-
-			if(!$stmt->execute()){
-				echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-			}
-			if(!$stmt->bind_result($s_m_name)){
-				echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-			}
-			while($stmt->fetch()){
-			 echo "<tr>\n<td>\n" . $s_m_name . "\n</td>\n<td>\n";
-			}
-			$stmt->close();
-			?>
-	</table>
-</div>
-
-<div>
-	<table style = "float: left">
-		<tr>
-			<td>Posts</td>
-		</tr>
-		<tr>
-			<td>Time Posted</td>
-			<td>Character Length</td>
-		</tr>
-		<tr>
-			<td> </td>
-			<td> </td>
-			<td> </td>
-		</tr>
-		
-			
-			<?php
-			if(!($stmt = $mysqli->prepare("SELECT post.time_posted, post.character_length FROM post"))){
-				echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
-			}
-
-			if(!$stmt->execute()){
-				echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-			}
-			if(!$stmt->bind_result($p_timeposted, $p_charlength)){
-				echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-			}
-			while($stmt->fetch()){
-			 echo "<tr>\n<td>\n" . $p_timeposted . "\n</td>\n<td>\n" . $p_charlength . "\n</td>\n<td>\n";
-			}
-			$stmt->close();
-			?>
-	</table>
-	
-</div>
-
-<div>
-	<table>
-		<tr>
-			<td>Content</td>
-		</tr>
-		<tr>
-			<td>Type</td>
-		</tr>
-		<tr>
-			<td> </td>
-			<td> </td>
-			<td> </td>
-		</tr>
-		
-			
-			<?php
-			if(!($stmt = $mysqli->prepare("SELECT content.type FROM content"))){
-				echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
-			}
-
-			if(!$stmt->execute()){
-				echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-			}
-			if(!$stmt->bind_result($c_type)){
-				echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-			}
-			while($stmt->fetch()){
-			 echo "<tr>\n<td>\n" . $c_type . "\n</td>\n<td>\n";
-			}
-			$stmt->close();
-			?>
-	</table>
-</div>
-<br><br>
-<div>
-	<table>
-		<tr>
-			<td>Feedback</td>
-		</tr>
-		<tr>
-			<td>Name</td>
-		</tr>
-		<tr>
-			<td> </td>
-			<td> </td>
-			<td> </td>
-		</tr>
-		
-			
-			<?php
-			if(!($stmt = $mysqli->prepare("SELECT feedback.name FROM feedback"))){
-				echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
-			}
-
-			if(!$stmt->execute()){
-				echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-			}
-			if(!$stmt->bind_result($f_name)){
-				echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-			}
-			while($stmt->fetch()){
-			 echo "<tr>\n<td>\n" . $f_name . "\n</td>\n<td>\n";
-			}
-			$stmt->close();
-			?>
-	</table>
-</div>
 <form action="business.php" method="post">
 
-		<fieldset>
-			<legend>Business</legend>
-            <label for="name">Business Name:</label><input type="text" name="name" id="name" />
-            <br class="clear" />
-            <label for="field">Field of Business:</label><input type="text" name="field" id="field" />
-            <br class="clear" />
-            <label for="location">Location</label><input type="text" name="location" id="location" />
-            <br class="clear" />
-			<legend>Social Media Platform</legend>
-            <select name = "SocialMediaPlatform">
-                <?php
-				if(!($stmt = $mysqli->prepare("SELECT id, name FROM social_media_platform"))){
-					echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
-				}
+    <fieldset>
+        <legend>Business</legend>
+        <label for="name">Business Name:</label><input type="text" name="name" id="name" />
+        <br class="clear" />
+        <label for="field">Field of Business:</label><input type="text" name="field" id="field" />
+        <br class="clear" />
+        <label for="location">Location</label><input type="text" name="location" id="location" />
+        <br class="clear" />
+        <label>Social Media Platform</label>
+        <select name = "SocialMediaPlatform">
+            <?php
+            if(!($stmt = $mysqli->prepare("SELECT id, name FROM social_media_platform"))){
+                echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+            }
 
-				if(!$stmt->execute()){
-					echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-				}
-				if(!$stmt->bind_result($id, $s_m_name)){
-					echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-				}
-				while($stmt->fetch()){
-					echo '<option value=" '. $id . ' "> ' . $s_m_name . '</option>\n';
-				}
-				$stmt->close();
-				?>
-            </select>
-		</fieldset>
-		
-		<p><input style = "float: left" type="submit" name="addbusiness" value="Add Business"/></p>
-		<p><input style = "float: left" type="submit" name="filterbusiness" value="Filter Business"/></p>
-		<p><input style = "float: left" type="submit" name="updatebusiness" value="Update Business"/></p>
-		<p><input type="submit" name="deletebusiness" value="Delete Business"/></p>
+            if(!$stmt->execute()){
+                echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+            }
+            if(!$stmt->bind_result($id, $s_m_name)){
+                echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+            }
+            while($stmt->fetch()){
+                echo '<option value=" '. $id . ' "> ' . $s_m_name . '</option>\n';
+            }
+            $stmt->close();
+            ?>
+        </select>
+    </fieldset>
+
+    <p><input style = "float: left" type="submit" name="addbusiness" value="Add Business"/></p>
+    <p><input style = "float: left" type="submit" name="filterbusiness" value="Filter Business"/></p>
+    <p><input style = "float: left" type="submit" name="updatebusiness" value="Update Business"/></p>
+    <p><input type="submit" name="deletebusiness" value="Delete Business"/></p>
+</form>
+<form action="post.php" method="post">
+    <fieldset>
+        <legend>Post</legend>
+        <label for="time_posted">Time Posted (YYYY-MM-DD hh:mm:ss):</label><input type="datetime" name="time_posted" id="time_posted" />
+        <br class="clear" />
+
+        <label for="time_posted">Character Length:</label><input type="text" name="character_length" id="character_length" />
+        <br class="clear" />
+        <label>Content Type</label>
+        <select name="ContentType">
+            <?php
+            if(!($stmt = $mysqli->prepare("SELECT id, type FROM content"))){
+                echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+            }
+
+            if(!$stmt->execute()){
+                echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+            }
+            if(!$stmt->bind_result($id, $c_type)){
+                echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+            }
+            while($stmt->fetch()){
+                echo '<option value=" '. $id . ' "> ' . $c_type . '</option>\n';
+            }
+            $stmt->close();
+            ?>
+        </select>
+
+        <br class="clear" />
+        <label>Feedback</label>
+        <select name="FeedbackType">
+            <?php
+            if(!($stmt = $mysqli->prepare("SELECT id, name FROM feedback"))){
+                echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+            }
+
+            if(!$stmt->execute()){
+                echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+            }
+            if(!$stmt->bind_result($id, $f_name)){
+                echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+            }
+            while($stmt->fetch()){
+                echo '<option value=" '. $id . ' "> ' . $f_name . '</option>\n';
+            }
+            $stmt->close();
+            ?>
+        </select>
+
+    </fieldset>
+    <p><input style = "float: left" type="submit" name="addpost" value="Add Post"/></p>
+    <p><input type="submit" name="filterpost" value="Filter Post"/></p>
+
 </form>
 
-<form action="post.php" method="post">
-		<fieldset>
-			<legend>Post</legend>
-            <label for="time_posted">Time Posted (YYYY-MM-DD hh:mm:ss):</label><input type="datetime" name="time_posted" id="time_posted" />
-            <br class="clear" />
 
-            <label for="time_posted">Character Length:</label><input type="text" name="character_length" id="character_length" />
-            <br class="clear" />
-			<legend>Content Type</legend>
-			<select name="ContentType">
-                <?php
-				if(!($stmt = $mysqli->prepare("SELECT id, type FROM content"))){
-					echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
-				}
+<div>
+    <table style="float: left">
+        <tr>
+            <th>Businesses</th>
+        </tr>
+        <tr>
+            <td>Name</td>
+            <td>Field</td>
+            <td>Location</td>
+        </tr>
+        <tr>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+        </tr>
 
-				if(!$stmt->execute()){
-					echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-				}
-				if(!$stmt->bind_result($id, $c_type)){
-					echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-				}
-				while($stmt->fetch()){
-					echo '<option value=" '. $id . ' "> ' . $c_type . '</option>\n';
-				}
-				$stmt->close();
-				?>
-            </select>
 
-            <legend>Feedback</legend>
-			<select name="FeedbackType">
-			<?php
-				if(!($stmt = $mysqli->prepare("SELECT id, name FROM feedback"))){
-					echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
-				}
+        <?php
+        if(!($stmt = $mysqli->prepare("SELECT business.name, business.field, business.location FROM business"))){
+            echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+        }
 
-				if(!$stmt->execute()){
-					echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-				}
-				if(!$stmt->bind_result($id, $f_name)){
-					echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-				}
-				while($stmt->fetch()){
-					echo '<option value=" '. $id . ' "> ' . $f_name . '</option>\n';
-				}
-				$stmt->close();
-				?>
-            </select>
+        if(!$stmt->execute()){
+            echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+        }
+        if(!$stmt->bind_result($name, $field, $location)){
+            echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+        }
+        while($stmt->fetch()){
+            echo "<tr>\n<td>\n" . $name . "\n</td>\n<td>\n" . $field . "\n</td>\n<td>\n" . $location . "\n</td>\n</tr>";
+        }
+        $stmt->close();
+        ?>
+    </table>
+</div>
 
-        </fieldset>
-		<p><input style = "float: left" type="submit" name="addpost" value="Add Post"/></p>
-		<p><input type="submit" name="filterpost" value="Filter Post"/></p>
+<div>
+    <table style="float: left">
+        <tr>
+            <th>Social Media Platform</th>
+        </tr>
+        <tr>
+            <td>Name</td>
+        </tr>
+        <tr>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+        </tr>
 
-	</form>
+
+        <?php
+        if(!($stmt = $mysqli->prepare("SELECT social_media_platform.name FROM social_media_platform"))){
+            echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+        }
+
+        if(!$stmt->execute()){
+            echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+        }
+        if(!$stmt->bind_result($s_m_name)){
+            echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+        }
+        while($stmt->fetch()){
+            echo "<tr>\n<td>\n" . $s_m_name . "\n</td>\n</tr>\n";
+        }
+        $stmt->close();
+        ?>
+    </table>
+</div>
+
+<div>
+    <table style="float: left">
+        <tr>
+            <th>Posts</th>
+        </tr>
+        <tr>
+            <td>Time Posted</td>
+            <td>Character Length</td>
+        </tr>
+        <tr>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+        </tr>
+
+
+        <?php
+        if(!($stmt = $mysqli->prepare("SELECT post.time_posted, post.character_length FROM post"))){
+            echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+        }
+
+        if(!$stmt->execute()){
+            echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+        }
+        if(!$stmt->bind_result($p_timeposted, $p_charlength)){
+            echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+        }
+        while($stmt->fetch()){
+            echo "<tr>\n<td>\n" . $p_timeposted . "\n</td>\n<td>\n" . $p_charlength . "\n</td>\n</tr>\n";
+        }
+        $stmt->close();
+        ?>
+    </table>
+
+</div>
+
+<div>
+    <table style="float: left">
+        <tr>
+            <th>&nbsp</th>
+        </tr>
+        <tr>
+            <td>Content Type</td>
+        </tr>
+        <tr>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+        </tr>
+
+
+        <?php
+        if(!($stmt = $mysqli->prepare("SELECT content.type FROM content"))){
+            echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+        }
+
+        if(!$stmt->execute()){
+            echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+        }
+        if(!$stmt->bind_result($c_type)){
+            echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+        }
+        while($stmt->fetch()){
+            echo "<tr>\n<td>\n" . $c_type . "\n</td>\n</tr>\n";
+        }
+        $stmt->close();
+        ?>
+    </table>
+</div>
+
+<div>
+    <table style="float: left">
+        <tr>
+            <th>&nbsp</th>
+        </tr>
+        <tr>
+            <td>Feedback</td>
+        </tr>
+
+        <tr>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+        </tr>
+
+
+        <?php
+        if(!($stmt = $mysqli->prepare("SELECT feedback.name FROM feedback"))){
+            echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+        }
+
+        if(!$stmt->execute()){
+            echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+        }
+        if(!$stmt->bind_result($f_name)){
+            echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+        }
+        while($stmt->fetch()){
+            echo "<tr>\n<td>\n" . $f_name . "\n</td>\n</tr>\n";
+        }
+        $stmt->close();
+        ?>
+    </table>
+</div>
+
+
+
+
 </body>
 </html>
